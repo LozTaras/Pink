82d65a4b82f8d93000e821175d84879c75a25f3c
Taras <t.lozinskiy.lv@gmail.com>
2018-08-22 15:16:41 +0300

    add filters functionality

---
+++ js/google-maps.js
@@ -0,0 +1,1 @@
+if(document.body.classList.contains("main"))function initMap(){map=new google.maps.Map(document.querySelector(".contact-us__google-map"),{center:{lat:59.9366,lng:30.3215},zoom:16});new google.maps.Marker({position:{lat:59.936102,lng:30.321534},map:map,icon:"img/icon-map-marker.png"})}
